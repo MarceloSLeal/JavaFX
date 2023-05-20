8e085fb42391ea10a76e7e940aa31688f38793dd
MarceloSLeal <marcelosleal@outlook.com>
2023-05-20 20:59:44 -0300

    AnchorPane, gerenciador de layout para ancorar os objetos em pontos de referência na tela, à esquerda, direita, cima, baixo ou outros pontos específicos

--- src/main/java/br/com/marcelo/javafx/layout/AppLayout.java
+++ src/main/java/br/com/marcelo/javafx/layout/AppLayout.java
@@ -2,13 +2,15 @@ package br.com.marcelo.javafx.layout;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AppLayout extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        Scene principal = new Scene(new TesteAnchorPane(), 800, 600);
+
+        primaryStage.setScene(principal);
         primaryStage.setTitle("Gerenciadores de Layout");
         primaryStage.show();
 
