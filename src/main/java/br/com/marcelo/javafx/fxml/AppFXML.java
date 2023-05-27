105fab0665994b22057e6e9bfe37b3ea98a71e2b
MarceloSLeal <marcelosleal@outlook.com>
2023-05-27 13:01:16 -0300

    Usando arquivo CSS em conjunto com fxml para estilizar os objetos

--- src/main/java/br/com/marcelo/javafx/fxml/AppFXML.java
+++ src/main/java/br/com/marcelo/javafx/fxml/AppFXML.java
@@ -10,10 +10,12 @@ public class AppFXML extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        String arquivoCSS = getClass().getResource("/br/com/marcelo/javafx/fxml/Login.css").toExternalForm();
         FXMLLoader fxmlLoader = new FXMLLoader(AppFXML.class.getResource("login.fxml"));
         GridPane raiz = FXMLLoader.load(fxmlLoader.getLocation());
 
         Scene cena = new Scene(raiz, 350, 400);
+        cena.getStylesheets().add(arquivoCSS);
 
         primaryStage.setResizable(false);
         primaryStage.setTitle("Tela de Login");
